6b1c29d8d7e2d69c24a13f275789fd9524060507
HYEONJI LEE <55472485+hyeonzii@users.noreply.github.com>
2024-03-21 19:03:18 +0900

    [Bronze I] Title: 단어 공부, Time: 196 ms, Memory: 146612 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,10 @@
+word = input().lower()
+word_list = list(set(word))
+cnt = [] 
+
+for i in word_list:
+    cnt.append(word.count(i))
+if cnt.count(max(cnt)) >= 2:
+    print("?")
+else:
+    print(word_list[(cnt.index(max(cnt)))].upper())
